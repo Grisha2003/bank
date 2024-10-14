5f235d91474207879ccbd0338e29da98b6a51905
Grisha <veryrich2022@gmail.com>
2024-10-15 02:14:39 +0600

    init: Rest.php finished

--- libs/Shared/Rest.php
+++ libs/Shared/Rest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Route;
+namespace Shared;
 
 /**
  * Description of Rest
@@ -8,24 +8,38 @@ namespace Route;
  * @author vladimir
  */
 class Rest {
-    private $namespace;
-    private $method;
-    private $inData;
-    private $outData;
+    private $namespace,
+            $error,
+            $method,
+            $request,
+            $class,
+            $status,
+            $params,
+            $url;
+   
+    
     public function __construct($data)
     {
-        $this->inData = $data;
+        $this->status = true;
+        $this->error = null;
+        $this->request = $data;
+        $this->url = $data['REQUEST_URI'];
+        $this->method = $this->getMethod();
+        $this->params = $this->getParams();
+        $this->class = $this->getClass();
+        $this->namespace = $this->getNamespace();
     }
     
-    private function readMethod($method)
+    private function getMethod()
     {
         $retMethod = null;
+        $method = $this->request['REQUEST_METHOD'];
         switch ($method) {
-            case 'POST':
-                $retMethod = 'create';
-                break;
             case 'GET':
                 $retMethod = 'read';
+                break;
+            case 'POST':
+                $retMethod = 'create';
                 break;
             case 'PUT':
                 $retMethod = 'edit';
@@ -34,15 +48,99 @@ class Rest {
                 $retMethod = 'delete';
                 break;
             default:
-                //ERROR
+                $this->status = false;
+                $this->error = 'Неверный вид запроса.';
         }
-        
         return $retMethod;
     }
     
-    private function readBody($body)
+    private function getParams()
     {
+        $retData = null;
+        if ($this->status) {
+            $retData = [];
+            $method = $this->request['REQUEST_METHOD'];
+            $url = $this->request['QUERY_STRING'];
+            if (isset($this->url)) {
+                switch ($method) {
+                    case 'GET':
+                        $retData = $this->openParamsProcessing($url);
+                        break;
+                    case 'POST':
+                        $retData = $this->closeParamsProcessing();
+                        break;
+                    case 'PUT':
+                        $retData = $this->closeParamsProcessing();
+                        break;
+                    case 'DELETE':
+                        $retData = $this->openParamsProcessing($url);
+                        break;
+                    default :
+                        $this->status = false;
+                        $this->error = 'Неверный вид запроса.';
+                }
+            } else {
+                $this->status = false;
+                $this->error = 'Пустая строка URL';
+            }
+        }
+        return $retData;
+    }
+    
+    private function openParamsProcessing($url)
+    {
+        if (is_string($url)) {
+            parse_str($url, $result);
+            $params = [];
+            foreach ($result as $key=>$value) {
+                $params[$key] = $value;
+            }
+        } else {
+            $this->status = false;
+            $this->error = 'Тип параметров неверный.';
+        }
+        return $this->status ? $params : null;
+    }
+    
+    private function closeParamsProcessing()
+    {
+        $postData = json_decode(file_get_contents("php://input"), true);
+        if (!is_array($postData)) {
+            $this->status = false;
+            $this->error = 'Ошибка json.';
+        }
         
+        return $this->status ? $postData : null;
+    }
+    
+    private function getNamespace()
+    {
+        $retNamespace = null;
+        if ($this->status) {
+            $arrUrl = explode('/', $this->url);
+            if (isset($arrUrl[1])) {
+                $retNamespace = $arrUrl[1];
+            } else {
+                $this->status = false;
+                $this->error = 'Неверный url.(namespace)';
+            }
+        }
+        return $retNamespace;
+    }
+    
+    private function getClass()
+    {
+        $retClass = null;
+        if ($this->status) {
+            $arrUrl = explode('/', $this->url);
+            if (isset($arrUrl[2])) {
+                $retClass = $arrUrl[2];
+            } else {
+                $this->status = false;
+                $this->error = 'Неверный url.(class)';
+            } 
+        }
+        return $retClass;
     }
     
     public function getData()
@@ -50,9 +148,12 @@ class Rest {
         $ret = [
             'namespace' => $this->namespace,
             'method' => $this->method,
-            'data' => $this->outData
+            'params' => $this->params,
+            'class' => $this->class,
+            'status' => $this->status,
+            'error' => $this->error
+            
         ];
-        
         return $ret;
     }
 }
