ccfcb5c77849c27f8dbaa3e9fd74522852e7b5c5
vladimir <vladimir@debian.mybuh.kz>
2024-10-16 15:10:39 +0500

    first in boy

--- ajax.php
+++ ajax.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'autoload.php';
 require 'libs/Shared/Rest.php';
 
@@ -16,10 +17,7 @@ global $outData;
 $rest = new \Shared\Rest($serverData);
 $data = $rest->getData();
 
-
-
-function dbConn($host, $username, $password, $database)
-{
+function dbConn($host, $username, $password, $database) {
     $connectDB = mysqli_connect($host, $username, $password, $database);
     return $connectDB;
 }
@@ -28,23 +26,26 @@ if ($data['status'] && empty($data['error'])) {
     $namespace = $data['namespace'];
     $class = $data['class'];
     $file = __DIR__ . DIRECTORY_SEPARATOR . 'libs' . DIRECTORY_SEPARATOR . $namespace . DIRECTORY_SEPARATOR . $class . '.php';
-    
+
     if (file_exists($file)) {
         $namespace = $data['namespace'];
         $class = $data['class'];
         $obj = '\\' . $namespace . '\\' . $class;
-        $db = dbConn('localhost:3306', 'p-345076_base', 'vovazero123', 'p-345076_base');
-        $object = new $obj($data['method'], $db);
-        $outData = $object->execute($data['params']);
+        try {
+            $db = dbConn('localhost:3306', 'p-345076_base', 'vovazero123', 'p-345076_base');
+            $object = new $obj($data['method'], $db);
+            $outData = $object->execute($data['params']);
+        } catch(Exception $e) {
+            $outData = ['error' => $e->getMessage()];
+        }
     } else {
         $outData = ['error' => '404'];
-      //  $outData = $file;
+        //  $outData = $file;
     }
 } else {
     $outData = $data['error'];
 }
 echo json_encode($outData);
-
 
 //
 //$obj = $namespace .$class;
