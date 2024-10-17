53ea4d82888b708875c6a3d58b2550dc170fac02
vladimir <vladimir@debian.mybuh.kz>
2024-10-17 14:35:25 +0500

    com

--- libs/Profile/Users.php
+++ libs/Profile/Users.php
@@ -37,7 +37,7 @@ class Users extends \Shared\Template {
                 'surname' => isset($data['surname']) && $data['surname'] != '' ? $data['surname'] : null,
                 'group' => isset($data['group']) && $data['group'] != '' ? $data['group'] : null,
                 'sum' => isset($data['sum']) && (int) $data['sum'] > 0 ? (int) $data['sum'] : null,
-                'pin' => isset($data['pin']) && (int) $data['pin'] > 0 ? (int) $data['pin'] : null
+                'pin' => is_int($data['pin']) && $data['pin'] >= 0 ? (int) $data['pin'] : null
             ],
             'read' => [
                 'pin' => isset($data['pin']) && (int) $data['pin'] > 0 ? (int) $data['pin'] : null
