8aaf76b6cb9f4853e0c687c6385f3adbad5bdf66
Vladimir <vladimir@debian.mybuh.kz>
2024-10-24 15:02:47 +0500

    add new modul Admin

--- libs/Shared/Rest.php
+++ libs/Shared/Rest.php
@@ -34,6 +34,7 @@ class Rest {
     {
         $retMethod = null;
         $method = $this->request['REQUEST_METHOD'];
+        echo $method;
         switch ($method) {
             case 'GET':
                 $retMethod = 'read';
