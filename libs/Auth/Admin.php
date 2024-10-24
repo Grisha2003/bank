2113a24daed106fe145d335cde8ef316f27c8ac3
Vladimir <vladimir@debian.mybuh.kz>
2024-10-24 16:06:29 +0500

    add new modul Admin

--- libs/Auth/Admin.php
+++ libs/Auth/Admin.php
@@ -67,7 +67,8 @@ class Admin extends \Shared\Template {
         $password = is_string($this->params['password']) ? $this->params['password'] : '';
         $login = is_string($this->params['login']) ? $this->params['login'] : '';
 
-        $strForHash = password_hash($password . $login, PASSWORD_DEFAULT);
+        //$strForHash = md5($password . $login);
+        $strForHash = md5($password);
         //$strForHash = '12312312';//md5($password . ':' . $login);
 
         $queryCheck = "SELECT id FROM `admin` WHERE hash = $strForHash";
